4eebaac728252e7371148b8041728e4812cbfb30
tiffany-929 <303889140@qq.com>
2024-09-09 16:55:08 +0800

    qualcommax: mk

--- target/linux/qualcommax/ipq50xx_32/target.mk
+++ target/linux/qualcommax/ipq50xx_32/target.mk
@@ -6,9 +6,11 @@ CPU_SUBTYPE:=neon-vfpv4
 DEFAULT_PACKAGES += \
 	kmod-gpio-button-hotplug \
 	uboot-envtools \
-	kmod-qca-nss-dp swconfig \
-	kmod-ath11k kmod-ath11k-ahb kmod-ath11k-pci wpad-basic-wolfssl
-
+	nss-firmware-ipq5018 \
+	kmod-ath11k kmod-ath11k-ahb kmod-ath11k-pci wpad-basic-wolfssl \
+	kmod-qca-nss-ecm kmod-qca-nss-drv kmod-qca-nss-ecm kmod-qca-nss-dp \
+	kmod-qca-nss-drv kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp \
+	
 define Target/Description
 	Build firmware images for Qualcomm Atheros IPQ50xx 32Bit based boards.
 endef
