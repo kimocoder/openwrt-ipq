1c449718439a7540ecac6cc2ac0a2584ef2aa549
kimocoder <christian@aircrack-ng.org>
2024-10-20 20:33:59 +0000

    refresh patches

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -17,6 +17,36 @@ define Device/EmmcImage
 	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
 endef
 
+define Device/jdcloud_ax1800-pro
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := JDCloud
+	DEVICE_MODEL := AX1800 Pro
+	DEVICE_DTS_CONFIG := config@cp03-c2
+	DEVICE_DTS := ipq6018-jdcloud-ax1800-pro
+	SOC := ipq6018
+	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax1800-pro kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
+	BLOCKSIZE := 64k
+	KERNEL_SIZE := 6144k
+	IMAGE/factory.bin := append-kernel | pad-to $${KERNEL_SIZE}  |  append-rootfs | append-metadata
+endef
+TARGET_DEVICES += jdcloud_ax1800-pro
+
+define Device/jdcloud_ax6600
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := JDCloud
+	DEVICE_MODEL := AX6600
+	DEVICE_DTS_CONFIG := config@cp03-c3
+	DEVICE_DTS := ipq6018-jdcloud-ax6600
+	SOC := ipq6010
+	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax6600 kmod-ath11k-pci ath11k-firmware-qcn9074 kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
+	BLOCKSIZE := 64k
+	KERNEL_SIZE := 6144k
+	IMAGE/factory.bin := append-kernel | pad-to $${KERNEL_SIZE}  |  append-rootfs | append-metadata
+endef
+TARGET_DEVICES += jdcloud_ax6600
+
 define Device/cambiumnetworks_xe3-4
        $(call Device/FitImage)
        $(call Device/UbiFit)
